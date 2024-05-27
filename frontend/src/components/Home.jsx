80156edc51c4ba3c773276b5a9676744af2ed132
PatrickBett <patrickbett018@gmail.com>
2024-05-27 05:05:35 +0300

    adding modals commit

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
 function Home() {
+
+    const handleTodo = () =>{
+        alert("Success")
+    }
+
+
+
+
   return (
     <>
       <h3 className='text-center'>Todo List</h3>
@@ -31,13 +39,40 @@ function Home() {
 
       
       <div className="modal-footer">
-        <button type="button" className="btn btn-primary" >Save</button>
+        <button type="button" className="btn btn-primary" onClick={handleTodo} >Save</button>
         <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
       </div>
 
     </div>
   </div>
 </div>
+
+
+
+{/* Delete Modal */}
+
+<div className="modal" id='deletemodal'>
+    <div className="modal-dialog">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h4>Delete Todo!!!</h4>
+                <button type='button' className='btn-close' data-bs-dismiss="modal"></button>
+            </div>
+
+            <div className="modal-body">
+                <label className='form-label'>Are you sure you want to delete?</label>
+            </div>
+
+            <div className="modal-footer">
+                <button type='button' className="btn btn-primary">No</button>
+                <button type='button' className="btn btn-danger " data-bs-dismiss="modal">Yes</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
 
       {/* Todo container */}
       <div className="container border rounded shadow mt-5 py-2 mb-5">
@@ -48,7 +83,7 @@ function Home() {
             <li className='mb-2 p-2 '>
               <label className='col-sm-3 me-2'>Read Django </label>
               <label className='col-sm-3 me-2'>27/05/2024</label>
-              <button className='btn btn-danger col-sm-2 me-2'>Delete</button>
+              <button className='btn btn-danger col-sm-2 me-2' data-bs-target='#deletemodal' data-bs-toggle='modal' >Delete</button>
               <button className='btn btn-primary col-sm-2'>Edit</button>
             </li>
             <li className='mb-2 p-2 '>
