21fd5df192392c9dc4d402dd694cc19fc0406fa7
PatrickBett <patrickbett018@gmail.com>
2024-05-27 06:45:19 +0300

    some stylings commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './footer.css'
 function Footer() {
   return (
-    <div className='fixed-bottom text-white text-center pb-0' id='footer'>
+    <div className='fixed-bottom text-center pb-0' id='footer'>
     
         <p>@dev.patrickbett2024</p>
     
